6a877c77aaac2646a1b7f6b0878fa44ccd43896c
Anselme GRUMBACH <agrumbac@e2r11p11.42.fr>
2017-02-19 22:04:17 +0100

    full stack support for my data struct

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/16 03:34:25 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/02/19 16:32:51 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/02/19 19:24:22 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ typedef	struct		s_point
 
 typedef struct		s_mlx
 {
+	void			*web;
+	int				web_x;
+	int				web_y;
 	void			*mlx_ptr;
 	void			*win;
 	void			*img;
@@ -35,6 +38,7 @@ typedef struct		s_mlx
 	int				bpp;
 	int				linesize;
 	int				endian;
+	int				zoom;
 }					t_mlx;
 
 char				*fdf_reader_checker(const char *file, int *y, int *x);
@@ -42,16 +46,16 @@ void 				fdf_parser(const int y, const int x, t_point web[y][x], \
 					const char *read);
 void 				fdf_painter(const int y, const int x, t_point web[y][x]);
 long				errors(const int err, const char *name);
-
 void				put_pixel(t_mlx *mlx, int x, int y, int color);
+void				painter(t_mlx *mlx);
 int					mouse(int button, int x, int y, void *param);
 int					keys(int keycode, void *param);
 
 # define WIN_NAME "fdf"
-# define WIN_W 1000
-# define WIN_H 1000
-# define IMG_W 1000
-# define IMG_H 1000
+# define WIN_W 1024
+# define WIN_H 632
+# define IMG_W 1024
+# define IMG_H 632
 
 # define DEFAULT_COLOR 0x00ff00
 
--- srcs/fdf_painter.c
+++ srcs/fdf_painter.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/18 01:15:52 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/02/19 16:58:27 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/02/19 22:00:46 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,42 @@ void				put_pixel(t_mlx *mlx, int x, int y, int color)
 
 	if (x >= IMG_W || y >= IMG_H)
 		errors(2, "Out of bounds");
-	i = mlx->linesize * y + x * 4;
+	i = mlx->linesize * y + x * (mlx->bpp / 8);
 	mlx->data[i] = color & 0xff;
 	mlx->data[++i] = color >> 8 & 0xff;
 	mlx->data[++i] = color >> 16;
 }
 
-static void			painter(const int y, const int x, t_point web[y][x], \
-					t_mlx *mlx)
+void				painter(t_mlx *mlx)
 {
 	int			y_count;
 	int			x_count;
+	t_point		(*web)[mlx->web_y][mlx->web_x];
 
+	web = mlx->web;//(t_point(*)[mlx->web_y][mlx->web_x])cast?
 	y_count = -1;
-	while (++y_count < y && (x_count = -1))
-		while (++x_count < x)
-			put_pixel(mlx, x_count * 2, y_count * 2, \
-				web[y_count][x_count].color);
+	while (++y_count < mlx->web_y && (x_count = -1))
+		while (++x_count < mlx->web_x)
+		{
+			put_pixel(mlx, x_count * mlx->zoom, y_count * mlx->zoom, \
+				(*web)[y_count][x_count].color);
+		}
 }
 
 void				fdf_painter(const int y, const int x, t_point web[y][x])
 {
 	t_mlx		mlx;
 
+	mlx.web_x = x;
+	mlx.web_y = y;
+	mlx.web = web;
 	mlx.mlx_ptr = mlx_init();
+	mlx.zoom = (IMG_W / x > IMG_H / y ? IMG_H / y : IMG_W / x);
 	mlx.win = mlx_new_window(mlx.mlx_ptr, WIN_W, WIN_H, WIN_NAME);
 	mlx.img = mlx_new_image(mlx.mlx_ptr, IMG_W, IMG_H);
-	mlx.data = mlx_get_data_addr(mlx.img, &(mlx).bpp, &(mlx).linesize, \
-			&(mlx).endian);
-	painter(y, x, web, &mlx);
+	mlx.data = mlx_get_data_addr(mlx.img, &(mlx.bpp), &(mlx.linesize), \
+			&(mlx.endian));
+	painter(&mlx);
 	mlx_key_hook(mlx.win, keys, &mlx);
 	mlx_mouse_hook(mlx.win, mouse, &mlx);
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img, 0, 0);
